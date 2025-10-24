6530ec6e6c896d08e7e3469510bcde9080d65e2d
zhouzihong <1635757679@qq.com>
2025-10-25 02:39:46 +0800

    Update room number in IndexView.vue to connect to the correct live session for improved streaming functionality.

--- DouyinLiveRecorder/dycast/src/views/IndexView.vue
+++ DouyinLiveRecorder/dycast/src/views/IndexView.vue
@@ -532,7 +532,7 @@ const saveCastToFile = function () {
 // 组件挂载后自动连接到指定房间
 onMounted(() => {
   // 设置房间号
-  roomNum.value = '63672926965';
+  roomNum.value = '24094593936';
 
   // 延迟1秒后自动连接，确保组件完全加载
   setTimeout(() => {
